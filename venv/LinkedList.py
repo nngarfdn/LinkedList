52341d0206bead1b8fdeccc6c57cdebf1a72ff07
nngarfdn <@nngarfdn123>
2020-03-09 12:39:49 +0700

    esdfesf

---
+++ venv/LinkedList.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  9 10:58:24 2020
+
+@author: hp
+"""
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setNext(self, newNext):
+        self.next = newNext
+
+    def setData(self, newData):
+        self.data = newData
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def countNode(self):
+        jml = 0
+        p = self.head
+        while p is not None:
+            jml = jml + 1
+            p = p.getNext()
+        return jml
+
+    def cekData(self, namaData):
+        p = self.head
+        if self.isEmpty():
+            return False
+        else:
+            while p is not None :
+                if namaData == p.getData() :
+                    return True
+                    break
+                elif p.getNext() == None:
+                    return False
+                p = p.getNext()
+
+    def cariData(self, namaData):
+        p = self.head
+        dataKe = 0
+        if self.isEmpty() :
+            return -1
+        else:
+            while p is not None:
+                dataKe = dataKe + 1
+                if namaData == p.getData():
+                    return dataKe
+                    break
+                elif p.getNext() == None:
+                    return False
+                p = p.getNext()
+
+
+    def sisipData(self, dataApa, nilaiData):
+        p = self.head
+
+        while p is not None :
+            if p.getData() == dataApa :
+                newNode = Node(nilaiData)
+                p.setNext(nilaiData)
+                break
+        p = p.getNext()
+
+    def isEmpty(self):
+        if self.head == None:
+            return True
+        else:
+            return False
+
+    def insertFirst(self, data):
+        nodeBaru = Node(data)
+        nodeBaru.setNext(self.head)
+        self.head = nodeBaru
+
+    def deleteFirst(self):
+        if self.isEmpty():
+            return None
+        else:
+            yangKeluar = self.head
+            self.head = yangKeluar.getNext()
+            return yangKeluar
+
+    def insertLast(self, data):
+        if self.isEmpty():
+            self.insertFirst(data)
+        else:
+            p = self.head
+            while p.getNext() is not None:
+                p = p.getNext()
+
+            nodeBaru = Node(data)
+            p.setNext(nodeBaru)
+
+    def deleteLast(self):
+        if self.isEmpty():
+            return None
+        elif self.head.getNext() == None:
+            yangKeluar = self.head
+            self.head = None
+            return yangKeluar
+        else:
+            p = self.head
+            while p.getNext().getNext() != None:
+                p = p.getNext()
+
+            yangKeluar = p.getNext()
+            p.setNext(None)  # sama dengan p.next = None
+            return yangKeluar
+
+    def printAll(self):
+        p = self.head
+        while p is not None:
+            print(p.data)
+            p = p.getNext()
+
+
+LL = LinkedList()
+LL.insertFirst("Nussa")
+LL.insertFirst("Rara")
+LL.insertLast("Anta")
+LL.sisipData("Rara", "Ariq")
+LL.printAll()
+print(LL.countNode())
+print(LL.cekData("Nussa"))
+print(LL.cekData("kjrhgkjre"))
+print(LL.cariData("Nussa"))
+print(LL.cariData("Anta"))
